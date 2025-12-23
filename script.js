0be4a898f02bfe9b9f4f6be515d0402c20f5db58
anuoluwa1234 <olukayodeanuoluwapo19@gmail.com>
2025-12-23 13:31:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("hireBtn").addEventListener("click", function () {
+    alert(
+        "I am passionate, disciplined, and constantly improving my skills.\n\n" +
+        "I enjoy learning new technologies and working on real-world projects."
+    );
+});
